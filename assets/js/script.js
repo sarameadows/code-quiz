f6345ee37fb4256dd327bc80d5bcc0e32d9a6833
Sara <sameads9@gmail.com>
2022-09-08 20:00:28 -0500

    html skeleton and quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// quiz questions
+var question = [
+    {
+        title: "Commonly used data types DO NOT include: ",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within __________.",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "parenthesis"
+    },
+    {
+        title: "Arrays in javascript can be used to store __________.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within __________ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    }
+];
