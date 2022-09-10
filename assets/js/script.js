0f400ae525ed2bafb04c4cf8d458b030a3896f26
Sara <sameads9@gmail.com>
2022-09-10 12:17:37 -0500

    setup javascript to navigate through quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,69 @@ const questions = [
         answer: "console log"
     }
 ];
+
+// basic quiz elements
+var score = 0;
+var questionIndex = 0;
+
+// start div elements
+const startDiv = document.getElementById("start");
+const startBtn = document.getElementById("start-button");
+
+// quiz question elements
+const quizDiv = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const answer1 = document.getElementById("answer-1");
+const answer2 = document.getElementById("answer-2");
+const answer3 = document.getElementById("answer-3");
+const answer4 = document.getElementById("answer-4");
+const answerCheck = document.getElementById("answer-check")
+
+// hide start screen and display quiz questions
+function displayQuiz() {
+    startDiv.style.display = "none";
+    quizDiv.style.display = "";
+    displayQuestion();
+}
+
+// display the question and answer choices
+function displayQuestion() {
+    questionTitle.textContent = questions[questionIndex].title;
+    answer1.textContent = questions[questionIndex].choices[0];
+    answer2.textContent = questions[questionIndex].choices[1];
+    answer3.textContent = questions[questionIndex].choices[2];
+    answer4.textContent = questions[questionIndex].choices[3];
+}
+
+// functions to check answer depending on selection
+function choice1() {checkAnswer(0);};
+function choice2() {checkAnswer(1);};
+function choice3() {checkAnswer(2);};
+function choice4() {checkAnswer(3);};
+
+// check the answer choice with true answer, update score, and index to next question
+function checkAnswer(answer) {
+    console.log(score);
+    if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
+        score = score + 5;
+        console.log(score);
+        answerCheck.style.display = "block";
+        answerCheck.textContent = "Correct!";
+    } else {
+        answerCheck.style.display = "block";
+        answerCheck.textContent = "Wrong!"
+    }
+
+    // set index to ask next question
+    questionIndex++;
+    if (questionIndex < questions.length) {
+        displayQuestion();
+    }
+}
+
+// button event listeners
+startBtn.addEventListener("click", displayQuiz);
+answer1.addEventListener("click", choice1);
+answer2.addEventListener("click", choice2);
+answer3.addEventListener("click", choice3);
+answer4.addEventListener("click", choice4);
