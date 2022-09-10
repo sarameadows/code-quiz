20c953c24853fcebbce16f067df8b335d0d92c00
Sara <sameads9@gmail.com>
2022-09-10 15:40:28 -0500

    display/save score to local storage functionality and high score display functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,10 @@ const questions = [
     }
 ];
 
+// header elements
+const viewHighScores = document.getElementById("view-high-scores");
+const time = document.getElementById("time");
+
 // basic quiz elements
 var score = 0;
 var questionIndex = 0;
@@ -43,6 +47,18 @@ const answer2 = document.getElementById("answer-2");
 const answer3 = document.getElementById("answer-3");
 const answer4 = document.getElementById("answer-4");
 const answerCheck = document.getElementById("answer-check")
+
+// individual score display elements
+const individualScoreDiv = document.getElementById('individual-score');
+const finalScoreDisplay = document.getElementById('final-score');
+const initialsInfo = document.getElementById('initials');
+const submitInitialsBtn = document.getElementById('submit-initials');
+
+// high score display elements
+const highScoreDiv = document.getElementById('high-score');
+const highScoreListDiv = document.getElementById('high-scores-list');
+const backBtn = document.getElementById('back');
+const clearBtn = document.getElementById('clear');
 
 // hide start screen and display quiz questions
 function displayQuiz() {
@@ -83,12 +99,103 @@ function checkAnswer(answer) {
     questionIndex++;
     if (questionIndex < questions.length) {
         displayQuestion();
+    } else {
+        endQuiz();
     }
-}
+};
 
-// button event listeners
+// when quiz is over display the individual score div to see and save score
+function endQuiz() {
+    quizDiv.style.display = "none";
+    finalScoreDisplay.textContent = score;
+    individualScoreDiv.style.display = "block";
+};
+
+// save initials and score to local storage
+function saveHighScore(event) {
+    event.preventDefault();
+
+    // get locally saved high scores and set up an array to add them to
+    var savedHighScores = localStorage.getItem("high scores");
+    var scoresArr;
+
+    // if there are no high scores set to blank array to add to, if there are, parse through the item to create scoresArr
+    if (savedHighScores === null) {
+        scoresArr = [];
+    } else {
+        scoresArr = JSON.parse(savedHighScores);
+    }
+
+    // add user score to the scores array
+    var userScore = {
+        initials: initialsInfo.value,
+        score: finalScoreDisplay.textContent
+    }
+    scoresArr.push(userScore);
+
+    // stringify the scores array and send to local storage
+    var highScoresString = JSON.stringify(scoresArr);
+    window.localStorage.setItem("high scores", highScoresString);
+
+    displayHighScores();
+};
+
+// display the high scores from local storage
+function displayHighScores() {
+    // navigate to high score page
+    individualScoreDiv.style.display = "none";
+    startDiv.style.display = "none";
+    quizDiv.style.display = "none";
+    highScoreDiv.style.display = "block";
+
+    // get high scores from local storage
+    var savedHighScores = localStorage.getItem("high scores");
+
+    // if there are no high scores notify user and return
+    if (!savedHighScores) {
+        highScoreListDiv.innerHTML = "No high scores";
+        return;
+    }
+
+    // parse high scores item
+    var highScores = JSON.parse(savedHighScores);
+
+    // go through each high score from local storage, create list item and append to high score list div
+    for(let i = 0; i < highScores.length; i++) {
+        var loadHighScore = document.createElement("p");
+        loadHighScore.classList.add("list-group-item");
+        loadHighScore.classList.add("list-group-item-info");
+        loadHighScore.innerHTML = `${i + 1}. ${highScores[i].initials}: ${highScores[i].score}`;
+        highScoreListDiv.appendChild(loadHighScore);
+    }
+
+};
+
+// button event listeners to start quiz and answer questions
 startBtn.addEventListener("click", displayQuiz);
 answer1.addEventListener("click", choice1);
 answer2.addEventListener("click", choice2);
 answer3.addEventListener("click", choice3);
 answer4.addEventListener("click", choice4);
+
+// route to saveHighScore to save score in local storage
+submitInitialsBtn.addEventListener("click", function(event){
+    saveHighScore(event);
+});
+
+// load high scores without going through quiz
+viewHighScores.addEventListener("click", function() {
+    displayHighScores();
+});
+
+// clear high scores in local storage
+clearBtn.addEventListener("click", function() {
+    window.localStorage.removeItem("high scores");
+    highScoreListDiv.innerHTML = "High scores cleared";
+});
+
+// take user back to beginning of quiz
+backBtn.addEventListener("click", function() {
+    startDiv.style.display = "block";
+    highScoreDiv.style.display = "none";
+});
